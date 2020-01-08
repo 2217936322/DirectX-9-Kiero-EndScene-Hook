e7f5fdcd9a7c603d493fde34b9b7cf727b476f44
Rdbo <57117082+rdbo@users.noreply.github.com>
2020-01-08 14:48:27 -0300

    Add files via upload

---
+++ DirectX 9 Kiero Hook/includes.h
@@ -0,0 +1,7 @@
+#pragma once
+#include <Windows.h>
+#include <d3d9.h>
+#include <d3dx9.h>
+#include "kiero/kiero.h"
+#include "kiero/minhook/include/MinHook.h"
+typedef long(__stdcall* EndScene)(LPDIRECT3DDEVICE9);
---
+++ DirectX 9 Kiero Hook/main.cpp
@@ -0,0 +1,34 @@
+#include "includes.h"
+EndScene oEndScene = NULL;
+
+long __stdcall hkEndScene(LPDIRECT3DDEVICE9 pDevice)
+{
+	return oEndScene(pDevice);
+}
+
+DWORD WINAPI MainThread(LPVOID lpReserved)
+{
+	bool attached = false;
+	do
+	{
+		if (kiero::init(kiero::RenderType::D3D9) == kiero::Status::Success)
+		{
+			kiero::bind(42, (void**)& oEndScene, hkEndScene);
+		}
+	} while (!attached);
+	return TRUE;
+}
+
+BOOL WINAPI DllMain(HMODULE hMod, DWORD dwReason, LPVOID lpReserved)
+{
+	switch (dwReason)
+	{
+	case DLL_PROCESS_ATTACH:
+		DisableThreadLibraryCalls(hMod);
+		CreateThread(nullptr, 0, MainThread, hMod, 0, nullptr);
+		break;
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
